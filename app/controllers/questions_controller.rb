22d4c1f0b753e99326b1f7d935190857bfbbc154
Maru <marutori.rurika@gmail.com>
2023-02-01 07:57:28 +0000

    add paging

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
   
   def index
     @q = Question.ransack(params[:q])
-    @questions = @q.result(distinct: true)
+    @questions = @q.result(distinct: true).page(params[:page]).per(5)
   end
   
   def solved
